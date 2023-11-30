5423b230f65598b9c9b0356e9bc86f667342fa63
Daniel Gorra <danielgorra7@gmail.com>
2023-11-30 17:34:09 -0500

    Finalized +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-  import type { PageData, ActionData } from "./$types";
+  import type { PageData } from "./$types";
 
   export let data: PageData;
 </script>
 
 <h1>Session authentication using Svelte</h1>
 
+<!-- Steps 1 and 2 -->
+<!-- <a href="/auth/signup">Sign up</a>
+<a href="/auth/login">Log in</a> -->
+
+<!-- Step 3 -->
 {#if data.authenticated}
   <form action="?/logout" method="POST">
     <button formaction="?/logout">Log out</button>
